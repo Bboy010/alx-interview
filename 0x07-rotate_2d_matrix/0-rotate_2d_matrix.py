099023691271b6051fa87800c625f209e95b0109
Bboy010 <hkoffianderson@gmail.com>
2023-09-13 17:00:06 -0700

    rotate 2D

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,24 +1,14 @@
 #!/bin/usr/env python3
 """ Rotation 2D interview"""
 
-
 def rotate_2d_matrix(matrix):
     n = len(matrix)
 
-    # Step 1: Transpose the matrix
+    # Step 1: Transpose the matrix (swap rows and columns)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Step 2: Reverse the order of elements in each row
+    # Step 2: Reverse each row
     for i in range(n):
-        matrix[i].reverse()
-
-
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-    rotate_2d_matrix(matrix)
-    print(matrix)
+        matrix[i] = matrix[i][::-1]
