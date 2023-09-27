a9c715624420e50605cba50c66b198f9db55b2c1
Bboy010 <hkoffianderson@gmail.com>
2023-09-27 13:51:22 -0700

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Count top edge
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Count bottom edge
+                if i == len(grid) - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Count left edge
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Count right edge
+                if j == len(grid[0]) - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
