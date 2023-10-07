ed4873e46a91a704390e4e2435d050db78afb1ff
Bboy010 <hkoffianderson@gmail.com>
2023-10-06 18:45:36 -0700

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,62 +1,45 @@
 #!/usr/bin/python3
 """
-Display the name of the winner
+Display the name of the player
+that win the most prime number
 """
 
 
 def isWinner(x, nums):
-    """
-    Determine the winner of the game
-    where x is the number of rounds and nums is an array of n
-Return: name of the player that won the most rounds
-If the winner cannot be determined, return None
-You can assume n and x will not be larger than 10000
-"""
-    def sieve(n):
-        # Sieve of Eratosthenes to generate prime numbers up to n
-        is_prime = [True] * (n + 1)
-        is_prime[0] = is_prime[1] = False
-        primes = []
-        for p in range(2, n + 1):
-            if is_prime[p]:
-                primes.append(p)
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-        return primes
+    def isPrime(n):
+        if n <= 1:
+            return False
+        if n <= 3:
+            return True
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
 
-    def canWin(n, primes):
-        """ Dynamic programming to check if
-        the current player can win for a given n
-        """
-        if n == 1:
-            return False  # Maria cannot make a move, so she loses
-
-        for prime in primes:
-            if prime > n:
-                break
-            if not canWin(n - prime, primes):
-                return True
-            """If there's a move leading to the opponent's
-            loss, Maria wins
-            """
-        return False
-
-    # Initialize the list of prime numbers up to the maximum n
-    max_n = max(nums)
-    primes = sieve(max_n)
+    def canWin(n):
+        if n <= 2:
+            return False
+        elif n % 2 == 0:
+            return True
+        else:
+            return not isPrime(n)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if canWin(n, primes):
+        if canWin(n):
             maria_wins += 1
         else:
             ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < ben_wins:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
